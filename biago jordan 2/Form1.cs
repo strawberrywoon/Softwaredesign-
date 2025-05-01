d0bbb2ecd72f93aa43980850b1efd49e34ea7098
strawberrywoon <jericktatad27@gmail.com>
2025-05-01 21:19:42 +0800

    Add files via upload

---
+++ biago jordan 2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace biago_jordan_2
+{
+    public partial class Musiczz : Form
+    {
+        public Musiczz()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(textBox1.Text);
+            double result = n1 * 0.99;
+            label4.Text = "The total download: " + result.ToString("F2") + " $";
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Assuming the intent is to close the form
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ biago jordan 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using biago_jordan_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace biago_jordan_2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Musiczz());
+        }
+    }
+}
